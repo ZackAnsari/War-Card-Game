5363601c795b5cd34768b95c929d805a3cb2dcb4
ZackAnsari <107903537+ZackAnsari@users.noreply.github.com>
2022-07-08 04:03:41 -0400

    Add files via upload

---
+++ WarCardGame.py
@@ -0,0 +1,142 @@
+import random
+suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+
+ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
+
+values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8,
+            'Nine':9, 'Ten':10, 'Jack':11, 'Queen':12, 'King':13, 'Ace':14}
+
+
+class Card():
+
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+        self.value = values[rank]
+
+    def __str__(self):
+        return self.rank + " of " + self.suit
+
+
+class Deck():
+
+    def __init__(self):
+
+        self.all_cards = []
+
+        for suit in suits:
+            for rank in ranks:
+                created_card = Card(suit, rank)
+
+                self.all_cards.append(created_card)
+
+    def shuffle(self):
+
+        random.shuffle(self.all_cards)
+
+    def Deal_One_Card(self):
+        return self.all_cards.pop()
+
+
+class Player():
+
+    def __init__(self, name):
+        self.name = name
+        self.all_cards = []
+
+    def remove_one(self):
+        return self.all_cards.pop(0)
+
+    def add_cards(self, new_cards):
+        if type(new_cards) == type([]):
+
+            self.all_cards.extend(new_cards)
+        else:
+            self.all_cards.append(new_cards)
+
+    def __str__(self):
+        return f'Player {self.name} has {len(self.all_cards)} cards.'
+
+
+# Game Set Up
+player1 = Player("One")
+player2 = Player("Two")
+
+new_deck = Deck()
+new_deck.shuffle()
+
+for x in range(26):
+    player1.add_cards(new_deck.Deal_One_Card())
+    player2.add_cards(new_deck.Deal_One_Card())
+
+game_on = True
+
+round_num = 0
+
+while game_on:
+    # prints current round number
+    round_num += 1
+    print("round: %s" % round_num)
+
+    # checks if player is out of cards
+    if len(player1.all_cards) == 0:
+        print("Player 1 is out of cards, Player 2 has won the war!")
+        game_on = False
+        break
+    if len(player2.all_cards) == 0:
+        print("Player 2 is out of cards, Player 1 has won the war!")
+        game_on = False
+        break
+
+    # adds card to current hand for comparison
+    player1_cards = []
+    player1_cards.append(player1.remove_one())
+
+    player2_cards = []
+    player2_cards.append(player2.remove_one())
+
+    # compares cards added to hand
+    at_war = True
+
+    while at_war:
+
+        # if player 1 wins
+        if player1_cards[-1].value > player2_cards[-1].value:
+
+            player1.add_cards(player1_cards)
+            player1.add_cards(player2_cards)
+
+            at_war = False
+        # if player 2 wins
+        elif player2_cards[-1].value > player1_cards[-1].value:
+
+            player2.add_cards(player2_cards)
+            player2.add_cards(player1_cards)
+
+            at_war = False
+
+        # if card value ties goes to war
+        else:
+            print("WAR!")
+
+            if len(player1.all_cards) < 5:
+                print("Player 1 is not able to continue, Player 2 wins")
+                game_on = False
+                break
+
+            elif len(player2.all_cards) < 5:
+                print("Player 2 is not able to continue, Player 1 wins")
+                game_on = False
+                break
+
+            else:
+                for num in range(5):
+                    player1_cards.append(player1.remove_one())
+                    player2_cards.append(player2.remove_one())
+
+
+
+
+
+
+
